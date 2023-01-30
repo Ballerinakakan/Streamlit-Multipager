a19d478a3e84bee07b17b31dfbc70d464bdca847
Lukas Alvarsson <lukas.alvarsson@gmail.com>
2023-01-30 18:12:47 +0100

    Seccond dataset worked on. Filtering works

--- Home.py
+++ Home.py
@@ -4,11 +4,12 @@ import numpy as np
 
 
 
-CSV_FILE = 'LoadingStatistics.csv'
+CSV_FILE_STATS = 'LoadingStatistics.csv'
+CSV_FILE_ROW_COUNTS = 'LoadingRowCounts.csv'
 
 
-def load_data(nrows):
-    df = pd.read_csv(CSV_FILE, nrows=nrows, delimiter=";")
+def load_data1(nrows):
+    df = pd.read_csv(CSV_FILE_STATS, nrows=nrows, delimiter=";")
     df.columns = ['Source System','Source Description', 'Source File', 'Source File Pattern', \
         'Source File Type', 'Run Date', 'Load Step', 'Load Step Date Time', \
         'Load Step Start Date Time', 'Load Step End Date Time', 'Load Step Status', \
@@ -16,8 +17,20 @@ def load_data(nrows):
 
     return df
 
+
+
+def load_data2(nrows):
+    df = pd.read_csv(CSV_FILE_ROW_COUNTS, nrows=nrows, delimiter=";")
+    df.columns = ['Sourcefile','Sourcefile Name', 'Sourcefile Type', \
+        'Sourcefile Size', 'Run Date', 'Model Object', 'Model Attribute', \
+        'Number Of Records Staged ', 'Number Of Records Processed ', 'Number Of Records Loaded ']
+
+    return df
+
 with st.spinner(text="Loading data, please wait..."):
-    data = load_data(100000)
+    data = load_data1(100000)
+    data2 = load_data2(100000)
 st.success('Done!')
 
 st.write(data)
+st.write(data2)
---
+++ pages/3_New_one.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time as tm
+import datetime as dt
+import dateutil.parser as dp
+import Home as hm
+import streamlit_timeline as st_tl
+
+
+basedf = hm.data2
+basedf['Run Date'] = pd.to_datetime(basedf['Run Date'])
+#curTime = dt.datetime.now()
+curTime = dt.datetime(2022, 3, 15, 1, 0, 0)
+
+option = st.selectbox('Time frame:', ('14 days', '30 days', '90 days'))
+
+if(option == '14 days'):
+    startTime = curTime - dt.timedelta(days=14)
+elif (option == '30 days'):
+    startTime = curTime - dt.timedelta(days=30)
+else:
+    startTime = curTime - dt.timedelta(days=90)
+
+timeFiltdf = basedf[ (basedf['Run Date'] >= startTime) & (basedf['Run Date'] <= curTime)]
+
+selected_source_file = st.selectbox('Select source File:', \
+    options= timeFiltdf['Sourcefile'].unique())
+
+selected_source_file
+
+selFildf = timeFiltdf[timeFiltdf['Sourcefile'] == selected_source_file]
+
+selFildf
+
+reduceddf = selFildf[['Sourcefile Name', 'Sourcefile Size', 'Run Date']].copy()
+reduceddf
+
+
+
+#nonCompFiltDF = timeFiltdf[ timeFiltdf['']]
